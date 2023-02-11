1a20ff555080c1abfbe0767fad02946782981d35
Carlos Molero <109365219+carlos-molero@users.noreply.github.com>
2023-02-11 01:59:46 +0100

    release: 0.1.0-beta (#4)

--- __tests__/aliases.test.js
+++ __tests__/aliases.test.js
@@ -2,22 +2,20 @@ import Mapper from '../src/index';
 import { faker } from '@faker-js/faker';
 import BasicSuperMarketDto from './__dtos__/BasicSuperMarketDto';
 
-describe('[UNIT] - Covers the use of map() chain function', function () {
+describe.only('[UNIT] - Covers the use of setAlias() chain function', function () {
   it('', function () {
     const dto = Mapper()
       .map(
         {
           address: faker.address.street(),
-          employees: faker.random.numeric(),
+          employeeNumber: faker.random.numeric(),
           location: faker.address.country(),
           systemPassword: faker.datatype.uuid(),
         },
         BasicSuperMarketDto,
       )
+      .setAlias('employeeNumber', 'employees')
       .get();
-    expect(dto.address).not.toBeUndefined();
-    expect(dto.location).not.toBeUndefined();
     expect(dto.employees).not.toBeUndefined();
-    expect(Object.keys(dto).length).toBe(3);
   });
 });
